81c85bd3b0cf226fe02d87bc9deedd1f0b7354b4
itzjustdanoob <editzzdevil514@gmail.com>
2026-07-17 09:53:57 +0530

    Fix SPA fallback route

--- index.js
+++ index.js
@@ -51,7 +51,10 @@ app.get('/settings', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')) })
+
+app.get('/*splat', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')) })
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
